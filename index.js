4dc7529e88cbf5ef31a903b9dff2ebf93a1bffec
canicemichael <canicemichael@gmail.com>
2022-02-13 09:34:59 +0100

    Update index.js .gitignore package.json

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-let done;
-let something;
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+//{useNewUrlParser:true, useUnifiedTopology:true }
+mongoose.connect(process.env.MONGO_URI, () => {
+    console.log("Connected to MongoDb");
+});
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8000, () => {
+    console.log('Backend Server is running!');
+})
