de81d5d256e4460a078283f6886784c90e952a17
canicemichael <canicemichael@gmail.com>
2022-03-21 10:06:37 +0100

    Update

--- routes/posts.js
+++ routes/posts.js
@@ -57,8 +57,8 @@ router.get('/:id', async (req, res) => {
 })
 
 //get all posts from a timeline
-router.get("/timeline/all", async (req, res) => {
-    const currentUser = await User.findById(req.body.userId);
+router.get("/timeline/:userId", async (req, res) => {
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
 
     const friendPosts = await Promise.all(
@@ -66,7 +66,7 @@ router.get("/timeline/all", async (req, res) => {
             return Post.find({ userId: friendId });
         })
     );
-    res.json(userPosts.concat(...friendPosts));
+    res.status(200).json(userPosts.concat(...friendPosts));
 })
 
 module.exports = router;
